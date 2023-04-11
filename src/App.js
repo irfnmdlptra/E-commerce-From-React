c6260fc6963d356af163291fbd4328a4035d15b6
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 15:29:33 +0800

    app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import  './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
