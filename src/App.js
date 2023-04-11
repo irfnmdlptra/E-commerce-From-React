5b07bbc9ef5823ea7b14a27ebb9e457feddec1c2
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 14:19:16 +0800

    Contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" element={<Layout/> }>
             <Route index element={<Home/>}/>
             <Route path="about" element={<About/>}/>
-            <Route path="contact" element={<About/>}/>
+            <Route path="contact" element={<Contact/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
