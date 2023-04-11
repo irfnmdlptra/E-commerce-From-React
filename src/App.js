6b6b28859d9b23a169825b19227103881f01b7c0
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 14:07:51 +0800

    Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          
+          <Route path="/" element={<Layout/> }></Route>
         </Routes>
       </BrowserRouter>
     </>
