f092864d7764dc0813a21f034a7cfa0364c81ac5
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 15:14:42 +0800

    link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,18 @@ import { Navlink, Link } from "react-router-dom";
 
 const Header = () => {
   return <>
-  <header className="header-top-strip">
-  
+  <header className="header-top-strip py-3">
+    <div className="container-xxl">
+      <div className="row">
+        <div className="col-lg-6">
+          <p>Download For App Mobile</p>
+        </div>
+        <div className="col-lg-6">
+          <p>Layanan <a href="tel:+62 89630025703">+62 89630056703</a>
+          </p>
+        </div>
+      </div>
+    </div>
   </header>
   </>
 };
