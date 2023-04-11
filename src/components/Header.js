d31a9ed5d10909ae2d8d753b6ef8fd9698596a57
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 17:06:19 +0800

    index.html

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
 import React from "react";
-import { Navlink, Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
-  return <>
-  <header className="header-top-strip py-3">
-    <div className="container-xxl">
-      <div className="row">
-        <div className="col-lg-6">
-          <p className="text-white">Download For App Mobile</p>
-        </div>
-        <div className="col-lg-6">
-          <p className="text-end text-white">
-            Layanan <a href="tel:+62 89630025703">+62 89630056703</a>
-          </p>
+  return (
+    <header className="header-top-strip py-3">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-lg-6">
+            <p className="text-white mb-0">Unduh Aplikasi Mobile</p>
+          </div>
+          <div className="col-lg-6">
+            <p className="text-end text-white mb-0">
+              Layanan <a href="tel:+6289630056703">+6289630056703</a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-  </>
+    </header>
+  );
 };
 
 export default Header;
