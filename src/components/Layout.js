3ce5c375d0acc57517f502313e76ec661815ae7f
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 14:04:11 +0800

    Outlet comp

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
-    return <div>Layout</div>
+    return <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
 }
 
 export default Layout
