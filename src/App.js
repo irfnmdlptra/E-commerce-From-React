1082d573e4023b73a4bbb1265d334c99c92cc21d
Irfnmdlptra <bangasrat@gmail.com>
2023-04-11 14:14:10 +0800

    Home Comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout/> }></Route>
+          <Route path="/" element={<Layout/> }>
+            <Route index element={<Home/>}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
