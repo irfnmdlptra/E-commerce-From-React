0cfe7e52683f7c4e7452fe9f8fd752ce66ee765a
Irfnmdlptra <bangasrat@gmail.com>
2023-04-12 09:31:58 +0800

    input group

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,40 @@ import { NavLink, Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-    <header className="header-top py-3 ">
-      <div className="container-xxl">
-        <div className="row">
-          <div className="col-lg-6">
-            <p className="text-white mb-0">Unduh Aplikasi Mobile</p>
-          </div>
-          <div className="col-lg-6">
-            <p className="text-end text-white mb-0">
-              Layanan <a href="tel:+6289630056703">+6289630056703</a>
-            </p>
+      <header className="header-top py-3 ">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-lg-6">
+              <p className="text-white mb-0">Unduh Aplikasi Mobile</p>
+            </div>
+            <div className="col-lg-6">
+              <p className="text-end text-white mb-0">
+                Layanan <a href="tel:+6289630056703">+6289630056703</a>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-    <header className="header-nav py-3 ">
+      </header>
+      <header className="header-nav py-3 ">
         <div className="container-xxl">
           <div className="row">
             <div className="col-2">
-              <h2 className="text-white">
-              <Link>Shopfrieger</Link>
+              <h2>
+                <Link className="text-white">Shopfrieger</Link>
               </h2>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div class="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                <span class="input-group-text" id="basic-addon2">
+                  @example.com
+                </span>
+              </div>
+            </div>
             <div className="col-5"></div>
           </div>
         </div>
-    </header>
+      </header>
     </>
   );
 };
