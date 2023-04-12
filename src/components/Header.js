45dfcda467180dce750ae99f46c554f46cc74ffa
Irfnmdlptra <bangasrat@gmail.com>
2023-04-12 09:10:26 +0800

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header-top-strip py-3 bg-primary">
+    <header className="header-top-strip py-3 ">
       <div className="container-xxl">
         <div className="row">
           <div className="col-lg-6">
